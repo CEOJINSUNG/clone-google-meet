c3d037a8878f5e8da4993184dacef77796c4400b
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-20 15:28:26 +0900

    Create Role.java
    사용자 역할 구분

---
+++ src/main/java/toy/project/clonegooglemeet/domain/Role.java
@@ -0,0 +1,15 @@
+package toy.project.clonegooglemeet.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    HOST("ROLE_HOST", "호스트"),
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
