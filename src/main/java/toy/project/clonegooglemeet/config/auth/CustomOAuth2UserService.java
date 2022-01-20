e052e2925bfce0a5518a1fee812ee0942a67cf3f
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-20 16:15:34 +0900

    Update CustomOAuth2UserService.java

--- src/main/java/toy/project/clonegooglemeet/config/auth/CustomOAuth2UserService.java
+++ src/main/java/toy/project/clonegooglemeet/config/auth/CustomOAuth2UserService.java
@@ -46,8 +46,9 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(Collections.singleton(
-                new SimpleGrantedAuthority(user.getRoleKey()),
-        ));
+                new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
 
     }
 
