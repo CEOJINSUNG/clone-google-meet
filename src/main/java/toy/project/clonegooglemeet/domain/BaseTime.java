afa791788802e24abaff5ab8708dfa622e2a5b30
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-20 15:28:19 +0900

    Create BaseTime.java
    자동 생성 및 수정 속성 제작

---
+++ src/main/java/toy/project/clonegooglemeet/domain/BaseTime.java
@@ -0,0 +1,22 @@
+package toy.project.clonegooglemeet.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    private LocalDate createdDate;
+
+    @LastModifiedDate
+    private LocalDate lastModifiedDate;
+}
