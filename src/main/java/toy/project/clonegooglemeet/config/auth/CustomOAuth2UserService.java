f812fd39b61322697eaaa8e02d311268d9366647
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-20 16:07:08 +0900

    Create CustomOAuth2UserService.java

---
+++ src/main/java/toy/project/clonegooglemeet/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,61 @@
+package toy.project.clonegooglemeet.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import toy.project.clonegooglemeet.config.auth.dto.OAuthAttributes;
+import toy.project.clonegooglemeet.config.auth.dto.SessionUser;
+import toy.project.clonegooglemeet.domain.User;
+import toy.project.clonegooglemeet.repository.UserRepository;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        // OAuth2 서비스 id
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        // OAuth2 로그인 진행 시 키가 되는 필드 값(PK)
+        String userNameAttributeName
+                = userRequest
+                .getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        // OAuth2UserService
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(Collections.singleton(
+                new SimpleGrantedAuthority(user.getRoleKey()),
+        ));
+
+    }
+
+    //유저 생성 및 수정 서비스 로직
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findUserByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return userRepository.save(user);
+    }
+}
