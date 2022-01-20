43c51ae96577022be7208b86f34d64f3dedde75d
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-20 15:49:09 +0900

    Update CloneGoogleMeetApplication.java

--- src/main/java/toy/project/clonegooglemeet/CloneGoogleMeetApplication.java
+++ src/main/java/toy/project/clonegooglemeet/CloneGoogleMeetApplication.java
@@ -2,7 +2,9 @@ package toy.project.clonegooglemeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CloneGoogleMeetApplication {
 
