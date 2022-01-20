468a6b1dea2bb2b519b293f7cc90bb9697773703
CEOJINSUNG <jinsung1048@gmail.com>
2022-01-20 15:28:39 +0900

    Create User.java
    사용자 도메인 설정

---
+++ src/main/java/toy/project/clonegooglemeet/domain/User.java
@@ -0,0 +1,47 @@
+package toy.project.clonegooglemeet.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @NoArgsConstructor
+public class User extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String email;
+    private String picture;
+
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
