0a0eed3daabcf2db7faa69a79b8a0a479735cf09
CEOJINSUNG <jinsung1048@gmail.com>
2022-07-10 20:28:00 +0900

    Feat : Websocket 설정 pub/sub 으로 변경

--- src/main/java/toy/project/clonegooglemeet/config/WebSocketConfig.java
+++ src/main/java/toy/project/clonegooglemeet/config/WebSocketConfig.java
@@ -18,10 +18,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         // WebSocket 을 연결하는 과정에서 메시지가 들어오는 경로를 설정하는 것으로
-        // /app 으로 접근하는 메시지만 핸들러로 라우팅을 함
-        // /topic 은 얘를 들어 클라이언트가 구독하고 있는 채팅방 A가 있을 때 /topic/A 을 구독하는 사람들만 소통이 가능함
-        // 따라서 /topic 은 주제의 Prefix 라고 보면 될 것이다.
-        registry.setApplicationDestinationPrefixes("/app")
-            .enableSimpleBroker("/topic");
+        // /pub 으로 접근하는 메시지만 핸들러로 라우팅을 함
+        // /sub 은 얘를 들어 클라이언트가 구독하고 있는 채팅방 A가 있을 때 /sub/A 을 구독하는 사람들만 소통이 가능함
+        // 따라서 /sub 은 주제의 Prefix 라고 보면 될 것이다.
+        registry.setApplicationDestinationPrefixes("/pub")
+            .enableSimpleBroker("/sub");
     }
 }
